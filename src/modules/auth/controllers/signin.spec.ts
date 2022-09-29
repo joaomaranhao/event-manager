fd5fbd479d55acbee6ca1d13b7ccea55e91d8b2a
João Maranhão <joaofelipemaranhao@gmail.com>
2022-09-29 19:06:23 -0300

    feat: ensure SignInController returns 400 if no password is provided

--- src/modules/auth/controllers/signin.spec.ts
+++ src/modules/auth/controllers/signin.spec.ts
@@ -41,4 +41,16 @@ describe('SignInController', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
+
+  it('should return 400 if no password is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_mail@mail.com'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: password'))
+  })
 })
