981b441ed35e8438ff6820bbeb97c16bce53e0cc
Joao Maranhao <joaofelipemaranhao@gmail.com>
2022-09-29 02:40:48 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/modules/auth/controllers/signup.controller.ts
+++ src/modules/auth/controllers/signup.controller.ts
@@ -1,7 +1,13 @@
 export class SignUpController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return {
+          statusCode: 400,
+          body: new Error(`Missing param: ${field}`)
+        }
+      }
     }
   }
 }
